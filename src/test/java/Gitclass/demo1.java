3e4d5c2594c465cfd1250be220422a271dfc1760
24-jayasri <mjayasri22@gmail.com>
2024-02-20 13:40:01 +0530

    New Project Automation Commit

--- src/test/java/Gitclass/demo1.java
+++ src/test/java/Gitclass/demo1.java
@@ -1,7 +1,17 @@
 package Gitclass;
 
 public class demo1 {
+	 private void method1() {
+	System.out.println("1");	
+	}
 	
-	
-	
+	 private void method2() {
+		 System.out.println("2");		
+		}
+		
+	 private void method3() {
+		 System.out.println("3");		
+		}
+		
+		
 }
--- src/test/java/Gitclass/demo2.java
+++ src/test/java/Gitclass/demo2.java
@@ -1,5 +1,14 @@
 package Gitclass;
 
 public class demo2 {
+	private void method4() {
+	System.out.println("4");
+
+	}
+	private void method5() {
+		System.out.println("5");	
+
+	}
+	
 
 }
